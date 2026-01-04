1d48c31b8a5cf0246242f33e54da46754bae30ef
Cihat Gündüz <jeehut@fline.dev>
2026-01-04 10:40:53 +0100

    Update dependencies & upgrade to HandySwiftUI 2.x

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
    name: "FlineDevKit",
-   platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .visionOS(.v1), .watchOS(.v9)],
+   platforms: [.iOS(.v18), .macOS(.v15), .tvOS(.v18), .visionOS(.v2), .watchOS(.v10)],
    products: [.library(name: "FlineDevKit", targets: ["FlineDevKit"])],
    dependencies: [
       // Apple
-      .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4"),
+      .package(url: "https://github.com/apple/swift-collections.git", from: "1.3.0"),
 
       // FlineDev
       .package(url: "https://github.com/FlineDev/ErrorKit.git", branch: "main"),
-      .package(url: "https://github.com/FlineDev/FreemiumKit.git", from: "1.16.0"),
+      .package(url: "https://github.com/FlineDev/FreemiumKit.git", from: "1.18.3"),
       .package(url: "https://github.com/FlineDev/HandySwift.git", branch: "main"),
       .package(url: "https://github.com/FlineDev/HandySwiftUI.git", branch: "main"),
       .package(url: "https://github.com/FlineDev/LinksKit.git", branch: "main"),
@@ -19,8 +19,8 @@ let package = Package(
       .package(url: "https://github.com/FlineDev/TranslateKit.git", branch: "main"),
 
       // 3rd-Party
-      .package(url: "https://github.com/pointfreeco/swift-identified-collections.git", from: "1.0.0"),
-      .package(url: "https://github.com/TelemetryDeck/SwiftSDK.git", from: "2.6.1"),
+      .package(url: "https://github.com/pointfreeco/swift-identified-collections.git", from: "1.1.1"),
+      .package(url: "https://github.com/TelemetryDeck/SwiftSDK.git", from: "2.11.0"),
    ],
    targets: [
       .target(
